09a38b032cee75bff965515203741f9f9781d14d
Foetus6907 <mghoubali@gmail.com>
2022-03-25 11:22:08 +0100

    add vuex

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,28 @@ import messages from "@intlify/vite-plugin-vue-i18n/messages";
 import { createApp } from "vue";
 import { createI18n } from "vue-i18n";
 import App from "./App.vue";
+import { createStore } from "vuex";
 
 const i18n = createI18n({
   locale: localStorage.locale || "en",
   messages,
 });
 
-createApp(App).use(i18n).mount("#app");
+// Create a new store instance.
+const store = createStore({
+  state() {
+    return {
+      count: 0,
+    };
+  },
+  mutations: {
+    increment(state: any) {
+      state.count++;
+    },
+  },
+});
+
+const app = createApp(App);
+app.use(i18n);
+app.use(store);
+app.mount("#app");
